fb11283659274cc95f5151e9fec613206fa2fabf
Erik Nuber <erik.nuber@yahoo.com>
2017-06-16 10:38:31 -0700

    creating component for showing book list

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,5 +1,8 @@
-function() {
+export default function() {
     return [
-
+        { title: 'JS: The Good Parts', pages: 101 },
+        { title: 'Harry Potter',  pages: 39},
+        { title:  'The Dark Tower',  pages: 85},
+        { title: 'Eloquent Ruby',  pages: 1}
     ]
 }

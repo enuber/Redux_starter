9d0b89a5a779afe3cb626014ee0ed788dd984662
Erik Nuber <erik.nuber@yahoo.com>
2017-06-20 12:35:58 -0700

    created chart for temperature based on city and have it place properly

--- src/containers/searchbar.js
+++ src/containers/searchbar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -9,6 +12,7 @@ export default class SearchBar extends Component {
         //we can do this instead of a fat arrow function in onChange this
         //binds 'this' to this function
         this.onInputChange = this.onInputChange.bind(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
     }
 
     onInputChange(event) {
@@ -19,7 +23,8 @@ export default class SearchBar extends Component {
 
     onFormSubmit(event) {
         event.preventDefault();
-
+        this.props.fetchWeather(this.state.term);
+        this.setState( {term: ''});
 
     }
 
@@ -40,3 +45,9 @@ export default class SearchBar extends Component {
         );
     }
 }
+
+// function mapDispatchToProps(dispatch) {
+//     return bindActionCreators({ fetchWeather}, dispatch);
+// }
+
+export default connect(null, { fetchWeather })(SearchBar);

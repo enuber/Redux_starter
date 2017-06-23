b91a22af9ef158ffa682503e839f279fbe02909d
Erik Nuber <erik.nuber@yahoo.com>
2017-06-22 17:50:59 -0700

    created index screen for posts for a blog

--- src/components/postsIndex.js
+++ src/components/postsIndex.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
+import _ from 'lodash';
 
 class PostsIndex extends Component {
+
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+
+    renderPosts() {
+        //this lodash method will map through an object like map normally would for an array.
+        //first arguement is the object and second is the function
+        return _.map(this.props.posts, post => {
+            return (
+               <li key={post.id} className="list-group-item">
+                   {post.title}
+               </li>
+            );
+        });
+    }
+
     render() {
         return (
             <div>
-                Posts Index
+                <h3>Posts</h3>
+                <ul className="list-group">
+                    { this.renderPosts() }
+                </ul>
             </div>
         )
     }
 }
 
-export default PostsIndex;
+function mapStateToProps(state) {
+    return { posts: state.posts }
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
